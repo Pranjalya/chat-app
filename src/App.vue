0237871ca4641dbc2ff8aa95238748b313d2fc9f
Pranjalya <pranjalyawarrior@gmail.com>
2020-07-15 23:51:38 +0530

    Add

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,21 @@
 <template>
   <div id="app">
     <header>
-      <h1>Millenials Chat</h1>
+      <h1>Millennials Chat</h1>
+      <p>USER : {{ displayUsername }}</p>
     </header>
 
     <section>
-      <div id="change_username">
+      <div v-if="!uNameAvailable" id="change_username">
         <input id="username" v-model="usrname" type="text" v-on:keypress.enter="setUsername" />
         <button id="send_username" type="button" v-on:click="setUsername">Set username</button>
       </div>
     </section>
 
     <section id="chatroom">
-      <div v-for="showMsg in messages" v-bind:key="showMsg.index" class="Message">
-        <div class="info-line">{{ showMsg.doc.usrname }}</div>
-        <div class="message">{{ showMsg.doc.msg }}</div>
+      <div v-for="showMsg in displayMessages" v-bind:key="showMsg.index" class="Message">
+        <div class="info-line"> {{ showMsg.doc.usrname }} </div>
+        <div class="message"> {{ showMsg.doc.msg }} </div>
       </div>
     </section>
 
@@ -38,52 +39,121 @@ export default {
   data() {
     return {
       localstore: {},
-      message: { "_id":"", "msg": "", "usrname": "", "date": "" },
+      message: { _id: "", msg: "", usrname: "", date: "" },
       msg: "",
       usrname: "",
-      messages: []
+      messages: [],
+      usrnameAvailable: true
     };
   },
 
   methods: {
     setUsername() {
-      this.message["usrname"] = this.usrname;
+      let uname = this.usrname;
+      console.log("Pushing ", uname, " to _local");
+      this.localstore
+        .get("_local/username")
+        .then(doc => {
+          console.log("Getting Username : ", doc);
+          doc.username = uname;
+          this.localstore
+            .put(doc)
+            .then(() => {
+              this.message.usrname = uname;
+              this.usrnameAvailable = true;
+            })
+            .catch(console.log);
+        })
+        .catch(err => {
+          //console.log(err);
+          if (err.status == 404) {
+            this.localstore
+              .put({ _id: "_local/username", username: uname })
+              .then(() => {
+                console.log("Getting Username : ", uname);
+                this.message.usrname = uname;
+                this.usrnameAvailable = true;
+              })
+              .catch(err => {
+                alert(err);
+              });
+          }
+        });
     },
     sendMessage() {
-      this.message["msg"] = this.msg;
-      this.postMessage();
+      var sMessage = this.msg;
+      this.postMessage(sMessage);
       this.msg = "";
     },
-    postMessage: function() {
-      this.message["date"] = new Date().toString();
-      this.message["_id"] = this.message.usrname+"_"+this.message.date;
+    updateStore(change) {
+      try{
+      console.log(" updateStore CHANGE = ", change);
+      this.messages.push(change);
+      console.log("AFTER PUSHING", this.messages[0].doc);
+       this.messages.sort(function(a, b) {
+         a = new Date(a.doc.date);
+         b = new Date(b.doc.date);
+         return a > b ? 1 : a < b ? -1 : 0;
+       });
+       console.log("After sorting, MESSAGES : ", this.messages);
+      }catch(err){console.log("updateStore", err)}
+    },
+    postMessage: function(sMessage) {
+      var timestamp = new Date().toString();
+      this.message["_id"] = this.message["usrname"] + "_" + timestamp;
       this.localstore
-        .put(this.message)
+        .put({ _id: this.message["_id"], msg: sMessage, usrname: this.message["usrname"], date: timestamp})
         .then(response => {
           console.log("Message posted");
-          this.loadMessages();
         })
         .catch(err => {
           console.log("Message not posted", err);
         });
     },
-    loadMessages() {
-      this.remotestore
+    loadAllMessages() {
+      this.localstore
         .allDocs({
           include_docs: true,
           attachments: true
         })
         .then(result => {
           console.log("RESULTS ARE :", result);
-          this.messages = result.rows;
-          this.messages.sort(function(a, b) {
-            a = new Date(a.doc.date);
-            b = new Date(b.doc.date);
-            return a > b ? 1 : a < b ? -1 : 0;
-          });
-          console.log("MESSAGES : ", this.messages);
+          if (result.rows) {
+            this.messages = result.rows;
+            this.messages.sort(function(a, b) {
+              a = new Date(a.doc.date);
+              b = new Date(b.doc.date);
+              return a > b ? 1 : a < b ? -1 : 0;
+            });
+            console.log("MESSAGES : ", this.messages);
+          }
         })
         .catch(err => {
+          console.log(err);
+        });
+    },
+    loadMessages() {
+      console.log("CALLING loadMessages");
+      this.localstore
+        .changes({
+          since: "now",
+          live: true,
+          include_docs: true
+        })
+        .on("change", change => {
+          console.log("Changes = ",change);
+          // change.id contains the doc id, change.doc contains the doc
+          if (change.deleted) {
+            console.log("Message deleted");
+          } else {
+            // document was added/modified
+            this.updateStore(change);
+            console.log("UPDATED");
+          }
+        })
+        .on("error", function(err) {
+          // handle errors
+          alert(1);
           console.log(err);
         });
     },
@@ -93,7 +163,7 @@ export default {
         retry: true
       })
         .on("change", function(info) {
-  
+          console.log("Changing: active");
         })
         .on("paused", function(err) {
           console.log("Replication: paused", err);
@@ -116,20 +186,40 @@ export default {
   computed: {
     displayMessages() {
       return this.messages;
+    },
+    uNameAvailable() {
+      return this.usrnameAvailable;
+    },
+    displayUsername() {
+      return this.message.usrname;
     }
   },
 
   mounted() {
     this.localstore = new PouchDB("localstore");
-    this.remotestore = new PouchDB(
+    this.localstore
+      .get("_local/username")
+      .then(item => {
+        console.log("USERNAME", item);
+        this.message.usrname = item.username;
+        this.usrnameAvailable = true;
+      })
+      .catch(err => {
+        console.log(err);
+        this.usrnameAvailable = false;
+      });
+    /*  this.remotestore = new PouchDB(
       "https://4f241480-c3c9-41c6-bb2e-98fd4cfe269e-bluemix:2d0f75eae437887122aec87b1225ad19a294f459beeb0a20fd69fb333cee4d4a@4f241480-c3c9-41c6-bb2e-98fd4cfe269e-bluemix.cloudantnosqldb.appdomain.cloud/simple-chat-app"
     );
-    this.loadMessages();
-    this.startRep();
-    setInterval(() => {
-      this.loadMessages();
-    }, 500)
-  },
+    
+  */ var i = 1;
+     if(i){
+     this.loadAllMessages();
+     i=0;
+     }
+     this.loadMessages();
+    //  this.startRep();
+  }
 };
 </script>
 
--- src/main.js
+++ src/main.js
@@ -4,6 +4,12 @@ import router from './router'
 import store from './store'
 import './registerServiceWorker'
 
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default-dark.css' // This line here
+
+Vue.use(VueMaterial)
+
 Vue.config.productionTip = false
 
 new Vue({
